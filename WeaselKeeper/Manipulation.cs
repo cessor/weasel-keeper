3c592c1d2d58ab0361813d7df3ecdedc410dde84
Johannes Hofmeister <github@account.cessor.de>
2014-06-05 22:47:26 +0200

    Added a feature to not break the semantics of the code from changing identifiers that don't exist. Identifiers are now replaced in all places with the same new identifier name (=rename). Before they were just randomly replaced, thus resulting in meaningless code for the abbreviated condition. The single condition had a different issue, here collisions could occur due to the same first characters, eg numbers and number (both cut to n, thus resulting in a collision). The programm now will attempt to find a random character from the identifier and use that as a replacement, if it already exists, it will try some more.

--- WeaselKeeper/Manipulation.cs
+++ WeaselKeeper/Manipulation.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
@@ -9,6 +11,7 @@ namespace WeaselKeeper
     internal class Manipulation
     {
         private static readonly Random Random = new Random();
+        private static readonly IdentifierSubstitutions Replacements = new IdentifierSubstitutions();
 
         public static void Normal(Snippet snippet)
         {
@@ -17,11 +20,13 @@ namespace WeaselKeeper
 
         public static void Abbrev(Snippet snippet)
         {
+            Replacements.Clear();
             ReplaceIdentifiersWith(snippet.Root, snippet.Identifiers, AbbreviateIdentifiers);
         }
 
         public static void Single(Snippet snippet)
         {
+            Replacements.Clear();
             ReplaceIdentifiersWith(snippet.Root, snippet.Identifiers, MakeSingleCharIdentifiers);
         }
 
@@ -34,23 +39,57 @@ namespace WeaselKeeper
 
         private static SyntaxToken AbbreviateIdentifiers(SyntaxToken a, SyntaxToken b)
         {
-            string identifierName = a.ValueText;
-            const int length = 3;
-            string mangeledName =
-                String.Concat(
-                    Enumerable.Range(0, length).Select(_ => RandomCharacter(identifierName).ToString().ToLower()));
-            return SyntaxFactory.Identifier(a.LeadingTrivia, mangeledName, a.TrailingTrivia);
+            var identifier = a.ValueText.Trim();
+            var abbreviatedIdentifier = IfNecessary(Abbreviate, identifier);
+            return SyntaxFactory.Identifier(a.LeadingTrivia, abbreviatedIdentifier, a.TrailingTrivia);
         }
-
-        private static char RandomCharacter(string identifierName)
-        {
-            return identifierName[Random.Next(identifierName.Length)];
-        }
-
+        
         private static SyntaxToken MakeSingleCharIdentifiers(SyntaxToken a, SyntaxToken b)
         {
-            string mangeledName = a.ValueText[0].ToString();
-            return SyntaxFactory.Identifier(a.LeadingTrivia, mangeledName, a.TrailingTrivia);
+            var identifier = a.ValueText;
+            var singleCharacterIdentifier = IfNecessary(CutToSingleCharacter, identifier);
+            return SyntaxFactory.Identifier(a.LeadingTrivia, singleCharacterIdentifier, a.TrailingTrivia);
+        }
+
+        private static string IfNecessary(Func<string, string> replace, string identifier)
+        {
+            if (ShouldNotBeReplaced(identifier))
+            {
+                return identifier;
+            }
+            if (Replacements.WasAlreadyReplaced(identifier))
+            {
+                return Replacements.For(identifier);
+            }
+            var replacement = Replacements.EnsureUniquenes(replace, identifier);
+            Replacements.Remember(identifier, replacement);
+            return replacement;
+        }
+
+        private static string CutToSingleCharacter(string identifier)
+        {
+            return RandomCharacter(identifier).ToString();
+        }
+
+        // Read in blacklist from the outside so that you don't have to recompile shit  
+        private static readonly IEnumerable<string> Blacklist = new[] { "List", "var", "Add" };
+
+        private static bool ShouldNotBeReplaced(string identifierName)
+        {
+            return Blacklist.Contains(identifierName);
+        }
+
+        private static string Abbreviate(string identifier)
+        {
+            // return ''.join(random.choice(identifier) for _ in range(3))... Sigh...
+            const int length = 3;
+            string abbreviation = String.Concat(Enumerable.Range(0, length).Select(_ => RandomCharacter(identifier).ToString().ToLower()));
+            return abbreviation;
+        }
+
+        private static char RandomCharacter(string identifier)
+        {
+            return identifier[Random.Next(identifier.Length)];
         }
     }
 }
---
+++ setup.bat
@@ -0,0 +1,2 @@
+rmdir ..\weasel-keeper-bin /S /Q
+xcopy WeaselKeeper\bin\Release ..\weasel-keeper-bin /Y /I
